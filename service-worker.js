1852ce285de6c946828d1141ff90a7e021af702b
skyit Smart Solutions AG <ip@skyit.ch>
2026-07-10 22:38:07 +0200

    Use JSON manifest for PWA install

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ const SHELL_ASSETS = [
   "/auth.js?v=20260622-4",
   "/app.js?v=20260707-1",
   "/qrcode-generator.js?v=20260622-4",
-  "/manifest.webmanifest?v=20260710-1",
+  "/manifest.json?v=20260710-1",
   "/icons/storage-retschwil-icon.svg"
 ];
 
