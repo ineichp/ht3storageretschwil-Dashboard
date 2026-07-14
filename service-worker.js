6ab51b12ca6098e74c10d1f5ccb06d4891a9e09f
skyit Smart Solutions AG <ip@skyit.ch>
2026-07-14 11:07:15 +0200

    Improve favicon and transparent header logo

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = "storage-retschwil-shell-v20260714-1";
+const CACHE_NAME = "storage-retschwil-shell-v20260714-2";
 const SHELL_ASSETS = [
   "/",
   "/index.html",
-  "/styles.css?v=20260714-1",
+  "/styles.css?v=20260714-2",
   "/auth.css?v=20260713-1",
   "/auth.js?v=20260713-1",
   "/app.js?v=20260707-1",
   "/qrcode-generator.js?v=20260622-4",
-  "/manifest.json?v=20260714-1",
-  "/icons/header-logo.png?v=20260714-1",
-  "/icons/favicon.png?v=20260714-1",
-  "/icons/android-icon.png?v=20260714-1"
+  "/manifest.json?v=20260714-2",
+  "/icons/header-logo.png?v=20260714-2",
+  "/icons/favicon.png?v=20260714-2",
+  "/icons/android-icon.png?v=20260714-2"
 ];
 
 self.addEventListener("install", event => {
